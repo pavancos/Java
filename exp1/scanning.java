b32a419e8d2c411dc6d87b510fd20c71c86ae08d
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-01-08 06:07:44 +0000

    added scanning

---
+++ exp1/scanning.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class scanning {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("enter the operator");
+        String op=sc.next();
+        switch (op.charAt(0)) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+            default:
+                break;
+        }
+        }
+
+    }
+}
+
