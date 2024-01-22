8a9abbe607ab0df4a018288055c79fa0dab4586e
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-01-22 21:58:34 +0530

    add swap

--- exp2/swap.java
+++ exp2/swap.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class swap {
-    
+    int a, b;
+    void S(swap obj){
+        int temp=obj.a;
+        obj.a=obj.b;
+        obj.b=temp;
+    }
+    public static void main(String[] args) {
+        Scanner Get=new Scanner(System.in);
+        System.out.println("Enter a, b");
+        swap obj=new swap();
+        obj.a=Get.nextInt();
+        obj.b=Get.nextInt();
+        obj.S(obj);
+        System.out.println("a="+obj.a+" b="+obj.b);
+        Get.close();
+    }
 }
