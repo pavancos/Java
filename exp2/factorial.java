95ab523e3c3147e977988f7af61db2e7504a51d1
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-01-22 06:24:07 +0000

    add factorial

---
+++ exp2/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class factorial {
+    public  static int factor(int a){
+        if(a==0) return 1;
+        else{
+            return a*factor(a-1);
+        }
+    } 
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int fact=factorial.factor(a);
+        System.out.println(fact);
+    }
+}
