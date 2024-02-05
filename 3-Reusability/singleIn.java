96cd3241048993cdd3f40b1856e1821a144ffc0d
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-02-05 11:19:56 +0530

    add:singleIN

---
+++ 3-Reusability/singleIn.java
@@ -0,0 +1,19 @@
+// 1. Java program to implement single inheritance
+class parent{
+    public void p(){
+        System.out.println("Parent");
+    }
+}
+class child extends parent{
+    public void c(){
+        System.out.println("Child");
+    }
+}
+class singleIn{
+    
+    public static void main(String[] args) {
+        child ch=new child();
+        ch.p();
+        ch.c();
+    }
+}
