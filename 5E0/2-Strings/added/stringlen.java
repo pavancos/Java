55bdc5bce8c6eed600e44034054b6f254441a2f1
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-02-10 12:25:22 +0000

    delete irregs

--- 5E0/2-Strings/added/stringlen.java
+++
@@ -1,9 +0,0 @@
-// Write a program to find the length of the string "refrigerator".
-import java.util.*;
-public class stringlen {
-    public static void main(String[] args) {
-        Scanner get = new Scanner(System.in);
-        String a = new String(get.next());
-        System.out.println("Length: "+a.length());
-    }
-}
