034a222cc409b13ee8785a1de4bd36864a19c703
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-02-18 12:42:43 +0000

    add:fruits

---
+++ 4-Polymorphism/Fruits.java
@@ -0,0 +1,33 @@
+class Apple {
+    void display() {
+        System.out.println("Inside Apple's display method");
+    }
+}
+
+class Banana extends Apple {
+    void display()
+    {
+        System.out.println("Inside Banana's display method");
+    }
+}
+
+class Cherry extends Apple {
+    void display() {
+        System.out.println("Inside Cherry's display method");
+    }
+}
+
+class Fruits_Dispatch {
+    public static void main(String args[]) {
+        Apple a = new Apple();
+        Banana b = new Banana();
+        Cherry c = new Cherry();
+        Apple ref;
+        ref = a;
+        ref.display();
+        ref = b;
+        ref.display();
+        ref = c;
+        ref.display();
+    }
+}
