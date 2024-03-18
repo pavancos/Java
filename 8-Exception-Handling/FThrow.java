b9d624a1891c8a0e24be90aa2552b53fd20a14ae
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-03-18 05:28:17 +0000

    re-throw

--- 8-Exception-Handling/FThrow.java
+++ 8-Exception-Handling/FThrow.java
@@ -1,7 +1,7 @@
 public class FThrow {
     public static void main(String[] args) {
         try {
-            throw new ArithmeticException("hello");
+            throw new ArithmeticException("Throw Msg");
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
         }
