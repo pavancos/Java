27694a78d643a80c7c4e81631b5ad48d2cdcdb2d
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-01 04:53:59 +0000

    fin:ja5

--- 9-MultiThreading/NoSync.java
+++ 9-MultiThreading/NoSync.java
@@ -1,23 +1,35 @@
 class Hello {
-    public void show() {
+    void display() {
         Thread t = Thread.currentThread();
-        for (int i = 0; i < 3; i++) {
-            System.out.print(t.getName() + " "); System.out.println("Hello");
-            try {Thread.sleep(1000);} catch (Exception e) {}
+        for (int i = 1; i <= 3; i++) {
+            System.out.print(t.getName()+" ");
+            System.out.println("Hello");
+            try {
+                t.sleep(1000);
+            } catch (Exception e) {
+            }
         }
     }
 }
-class T extends Thread{
+
+class T extends Thread {
     Hello hlo;
-    T(Hello hlo){ this.hlo=hlo; }
-    public void CallShow(){
-        this.hlo.show();
+    T(Hello hlo) {
+        this.hlo = hlo;
+    }
+    public void run() {
+        hlo.display();
     }
 }
-public class NoSync {
-    public static void main(String[] args) {
+
+class NoSync {
+    public static void main(String args[]) {
         Hello hlo=new Hello();
-        T t1=new T(hlo); T t2=new T(hlo); T t3=new T(hlo);
-        t1.start();t2.start();t3.start();
+        T t1 = new T(hlo);
+        T t2 = new T(hlo);
+        T t3 = new T(hlo);
+        t1.start();
+        t2.start();
+        t3.start();
     }
 }
