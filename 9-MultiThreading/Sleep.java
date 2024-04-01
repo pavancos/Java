eb09b9eadfd80fad9940246afea0ea7226258ed7
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-01 04:09:22 +0000

    add:ps2

--- 9-MultiThreading/Sleep.java
+++ 9-MultiThreading/Sleep.java
@@ -1,16 +1,20 @@
 class Hello{
     public void ShowHello() {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++){
+            try{Thread.sleep(1000);}catch(Exception e){};
             System.out.println("Hello");
+        }
     }
 }
 class Hi{
     public void ShowHi() {
-        for (int i = 0; i < 5; i++) 
+        for (int i = 0; i < 5; i++){
+            try{Thread.sleep(1000);}catch(Exception e){};
             System.out.println("Hi");
+        }
     }
 }
-public class NoThread {
+public class Sleep {
     public static void main(String[] args) {
         Hello hlo = new Hello();
         Hi hi = new Hi();
