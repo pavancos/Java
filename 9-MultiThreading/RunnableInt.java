a490226cd2f75561aafd60b946c9a733c1250e8a
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-01 04:28:49 +0000

    out:add4

---
+++ 9-MultiThreading/RunnableInt.java
@@ -0,0 +1,32 @@
+class Hi implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread());
+            System.out.println("Hi");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
+
+class Hello implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread());
+            System.out.println("Hello");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
+public class RunnableInt {
+    public static void main(String[] args) {
+        Hi hi=new Hi();Hello hlo=new Hello();
+        Thread tHi=new Thread(hi);Thread tHlo=new Thread(hlo);
+        tHi.start();tHlo.start();
+    }
+}
