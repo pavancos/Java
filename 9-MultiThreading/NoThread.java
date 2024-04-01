ee48e9d808aacb5ed71eba94dcb7088c7e1e9bb5
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-01 04:05:21 +0000

    out:p1

--- 9-MultiThreading/NoThread.java
+++ 9-MultiThreading/NoThread.java
@@ -1,1 +1,20 @@
- 
+import java.util.*;
+class Hello{
+    public void ShowHello() {
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello");
+    }
+}
+class Hi{
+    public void ShowHi() {
+        for (int i = 0; i < 5; i++) 
+            System.out.println("Hi");
+    }
+}
+public class NoThread {
+    public static void main(String[] args) {
+        Hello hlo = new Hello();
+        Hi hi = new Hi();
+        hlo.ShowHello();hi.ShowHi();
+    }
+}
