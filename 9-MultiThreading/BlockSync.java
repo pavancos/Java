bf301c28e3cfe5e1a83de4bd551ab8d3e11ee892
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-01 05:04:59 +0000

    add:outs blovk

--- 9-MultiThreading/BlockSync.java
+++ 9-MultiThreading/BlockSync.java
@@ -1,5 +1,32 @@
-package 9-MultiThreading;
-
-public class BlockSync {
-    
+class Hello {
+    public void display() {
+        Thread t = Thread.currentThread();
+        for (int i = 1; i <= 3; i++) {
+            synchronized(this){
+                System.out.print(t.getName()+" ");
+                System.out.println("Hello");
+                try {
+                    t.sleep(1000);
+                } catch (Exception e) {}
+            }
+        }
+    }
+}
+class T extends Thread {
+    Hello hlo;
+    T(Hello hlo) {
+        this.hlo = hlo;
+    }
+    public void run() {
+        hlo.display();
+    }
+}
+class BlockSync {
+    public static void main(String args[]) {
+        Hello hlo=new Hello();
+        T t1 = new T(hlo);
+        T t2 = new T(hlo);
+        T t3 = new T(hlo);
+        t1.start();t2.start();t3.start();
+    }
 }
