f9aba8796f0e03e3922a484a2fe9ae9883d00707
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-15 04:51:41 +0000

    add:emp

--- 10-Collections/HashSet/Empty.java
+++ 10-Collections/HashSet/Empty.java
@@ -1,10 +1,11 @@
 import java.util.*;
-public class Size {
+public class Empty {
     public static void main(String[] args) {
         HashSet<String>Colors=new HashSet<>();
         Colors.add("yellow");Colors.add("blue");
         Colors.add("black");Colors.add("red");
-        Colors.add("white");System.out.println(Colors);
-        System.out.println(Colors.size());
+        Colors.add("white");
+        System.out.println(Colors.size());Colors.clear();
+        System.out.println("After Clear: "+Colors.size());
     }    
 }
