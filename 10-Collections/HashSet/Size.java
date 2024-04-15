53452056a7fe88b8d3eda4e8652d93e986eb21ca
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-15 04:45:28 +0000

    add:size

---
+++ 10-Collections/HashSet/Size.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Size {
+    public static void main(String[] args) {
+        HashSet<String>Colors=new HashSet<>();
+        Colors.add("yellow");Colors.add("blue");
+        Colors.add("black");Colors.add("red");
+        Colors.add("white");System.out.println(Colors);
+        System.out.println(Colors.size());
+    }    
+}
