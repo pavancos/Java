930da57c1b9dd876da4704eff8fa2630709c5cf7
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-05-03 16:03:42 +0530

    Update typeOL.java

--- 04-Polymorphism/typeOL.java
+++ 04-Polymorphism/typeOL.java
@@ -4,19 +4,16 @@ public class typeOL {
         s = num1 + num2;
         return s;
     }
-
     static double sum(int num1, float num2) {
         float s = 0;
         s = num1 + num2;
         return s;
     }
-
     static double sum(int num1, double num2) {
         double s = 0;
         s = num1 + num2;
         return s;
     }
-
     public static void main(String[] args) {
         double result = 0;
         result = sum(1, 2);
