42bb696ade2d0a32316c6db0a89eec6a2cb4e978
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-05-03 16:01:19 +0530

    Update CallInterface.java

--- 06-Interfaces/CallInterface.java
+++ 06-Interfaces/CallInterface.java
@@ -1,19 +1,15 @@
 import java.util.*;
-
 interface sample {
     void call();
 }
-
 class CallSample implements sample {
     public void call() {
         System.out.println("Interface\n");
     }
 }
-
 public class CallInterface {
     public static void main(String[] args) {
         CallSample s = new CallSample();
         s.call();
     }
-
 }
